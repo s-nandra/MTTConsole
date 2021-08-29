7b7da213908b77768637ce4a19767b1ed6a7f2c2
Satnam Nandra <SatnamNandra@yahoo.com>
2021-08-29 12:57:16 +0100

    first commit

---
+++ MTTConsole/ConsumeEventSync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MTTConsole
+{
+    public class ConsumeEventSync
+    {
+        public void GetAllEventData(string apiCall) //Get All Events Records  
+        {
+            
+            using (var client = new WebClient()) //WebClient  
+            {
+                client.Headers.Add("Content-Type:application/json"); //Content-Type  
+                client.Headers.Add("Accept:application/json");
+                //var result = client.DownloadString("http://localhost:53590/api/Products"); //URI  
+                var result = client.DownloadString(apiCall); //URI  
+                Console.WriteLine(Environment.NewLine + result);
+            }
+        }
+    }
+}
---
+++ MTTConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MTTConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConsumeEventSync objsync = new ConsumeEventSync();
+
+            var apiCall = "http://localhost:53590/api/Products";
+            Console.WriteLine(Environment.NewLine + "Get all products..."+ Environment.NewLine);
+            objsync.GetAllEventData(apiCall);
+
+
+            apiCall = "http://localhost:53590/api/products/GetFeaturedProducts/1";
+            Console.WriteLine(Environment.NewLine + "Get  Featured Products..." + Environment.NewLine);
+            objsync.GetAllEventData(apiCall);
+
+            string[] categories = { "Electronics", "Fitness", "Garden", "Home","Toys" };
+            
+             
+            foreach (string value in categories)
+            {
+                Console.WriteLine(Environment.NewLine + "Get products by category ..."+ value + " " + Environment.NewLine);
+               
+                apiCall = "http://localhost:53590/api/products/GetByCategory/" + value;
+                objsync.GetAllEventData(apiCall);
+
+            }
+
+        }
+    }
+}
